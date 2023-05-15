2b5f8fb6a1ec216481824473e061e88ff2434375
Martin Nyhus <martin@nyhus.dev>
2023-05-15 11:28:56 +0200

    download_tiles: update with new mount point

--- download_tiles.py
+++ download_tiles.py
@@ -29,7 +29,7 @@ def download_tile(url: str, dest: pathlib.Path):
 
 
 def download_single_tile(nib_api_key, z, x, y):
-    nib_dest = pathlib.Path('/mnt/server-NiB/{}/{}/{}.jpeg'.format(z, x, y))
+    nib_dest = pathlib.Path('/mnt/NiB/{}/{}/{}.jpeg'.format(z, x, y))
     if not nib_dest.exists():
         start = time.time()
 
@@ -79,7 +79,7 @@ def for_each_tile(west, south, east, north, zoom_levels, func):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--database', default='/mnt/server-NiB/tiles.db')
+    parser.add_argument('--database', default='/mnt/NiB/tiles.db')
     parser.add_argument('--NiB-key', type=str, default="secret/NiB_key.json")
     parser.add_argument('--population', default='population.csv')
     parser.add_argument('--min-population', default=10000, type=int)
