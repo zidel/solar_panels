2dc8c8f064dfb7537597259489759ff69eedcabf
Martin Nyhus <martin@nyhus.dev>
2023-05-20 18:53:13 +0200

    from_osm: update for new database format

--- from_osm.py
+++ from_osm.py
@@ -1,9 +1,13 @@
+import argparse
 import datetime
 import json
 import requests
 import sqlite3
 
+import tqdm
+
 import database
+import download_tiles
 import util
 
 
@@ -29,33 +33,40 @@ def get_points_from_overpass():
     ''')
     data = json.loads(result)
 
+    points = []
     for element in data['elements']:
         if 'center' in element:
             # {'type': 'way', 'id': 1023439043, 'center': {'lat': 59.9195945, 'lon': 10.632018}}
-            yield element['center']
+            points.append(element['center'])
         elif 'lat' in element and 'lon' in element:
             # {'type': 'node', 'id': 9439577222, 'lat': 59.9362591, 'lon': 10.9199213}
-            yield {'lat': element['lat'], 'lon': element['lon']}
+            points.append({'lat': element['lat'], 'lon': element['lon']})
         else:
             raise RuntimeError(str(element))
 
+    return points
+
 
 def main():
-    db = database.Database('data/tiles.db')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--database', default='data/tiles.db')
+    parser.add_argument('--NiB-key', type=str, default="secret/NiB_key.json")
+    args = parser.parse_args()
 
-    for point in get_points_from_overpass():
+    db = database.Database(args.database)
+    nib_api_key = util.load_key(args.NiB_key)
+
+    for point in tqdm.tqdm(get_points_from_overpass()):
         xtile, ytile = util.deg2tile(point['lat'], point['lon'], 18)
+        _, tile_hash = download_tiles.download_single_tile(
+                nib_api_key, 18, xtile, ytile)
+
         try:
             with db.transaction() as c:
-                database.add_tile(c, 18, xtile, ytile)
-
+                database.add_tile(c, 18, xtile, ytile, tile_hash)
                 now = datetime.datetime.now()
                 timestamp = now.isoformat()
-                database.write_score(c, 18, xtile, ytile, 1.0, 'OSM', timestamp)
-
-                c.execute('''delete from with_solar
-                             where z = 18 and x = ? and y = ?''',
-                          [xtile, ytile])
+                database.write_score(c, tile_hash, 1.0, 'OSM', timestamp)
         except sqlite3.IntegrityError as e:
             pass
 
